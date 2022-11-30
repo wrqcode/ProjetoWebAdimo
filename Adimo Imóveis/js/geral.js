2fd48580c3f5851201265c1f3a1a577bd7caf4f4
WilliamRibeiroQueiroz <williamrs.12@gmail.com>
2022-11-29 22:26:02 -0300

    Ajuste nas telas

---
+++ Adimo Imóveis/js/geral.js
@@ -0,0 +1,24 @@
+
+$('#home-menu').click(function(){
+    window.location.href = "../HTML/index.html"
+});
+
+$('#about-menu').click(function(){
+    window.location.href = "../HTML/sobre.html"
+});
+
+$('#services-menu').click(function(){
+    window.location.href = "../HTML/imoveis.html"
+});
+
+$('#team-menu').click(function(){
+    window.location.href = "../HTML/trabalheconosco.html"
+});
+
+$('#portfolio-menu').click(function(){
+    window.location.href = "../HTML/imoveis.html"
+});
+
+$('#contact-menu').click(function(){
+    window.location.href = "../HTML/contatos.html"
+});
